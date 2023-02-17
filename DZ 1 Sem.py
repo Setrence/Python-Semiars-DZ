d962c8e5b2e8b8fd1b06edd936ad57a7df994b98
Evgeniy <241126@mail.ru>
2023-02-17 13:36:10 +0300

    Commit zadanie 1

---
+++ DZ 1 Sem.py
@@ -0,0 +1,16 @@
+#Задание № 1
+#Найдите сумму цифр трехзначного числа.
+#Пример:
+#123 -> 6 (1 + 2 + 3)
+#100 -> 1 (1 + 0 + 0)
+
+# number = int (input('Введите 3х значное число: '))
+# number_1 = number % 10
+# number = number // 10
+# number_2 = number % 10
+# number = number // 10
+# number_3 = number
+#
+# sum_numbers = number_1 + number_2 + number_3
+#
+# print(f'Сумма цифр введенного трехзначного числа = {sum_numbers} ({number_3} + {number_2} + {number_1})')
